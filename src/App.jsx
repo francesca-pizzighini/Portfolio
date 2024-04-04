96382b780cb0ff241ddaab4b65b9a7d551a11fd0
francesca-pizzighini <133757414+francesca-pizzighini@users.noreply.github.com>
2024-04-04 19:40:23 +0200

    home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Helmet, HelmetProvider } from "react-helmet-async"
+import "./app.scss"
 
 function App() {
 
@@ -18,7 +19,15 @@ function App() {
           ></meta>
         </Helmet>
       </HelmetProvider>
-      home page
+
+      <section class="biography">
+        <p class="bio">
+          Born in 1999 in Bergamo, Italy.  <br/>
+          Since I was a child I looked with amazement at computers and technology but I really approached coding only by chance, taking a course that seemed interesting at university even though it was quite far from my field of study. Now I have a bachelor's degree in psychology, but I've fallen in love with web development. <br/>
+          I realized pretty fast that what I wanted to do in my life was programmer’s routine: setting a goal, struggling when things don't work out, and continuing until the problem is solved. So I decided to roll up my sleeves and start to really study in deep what it means to build website from scratch, and I chose Start2Impact University for this interesting journey. <br/>
+          Now, even though I still have a long way to go and I can’t wait to learn more and be better, I finally feel prepared to call myself a front-end developer. You can see my works and judge by yourself in the projects page.
+        </p>
+      </section>
     </div>
   )
 }
