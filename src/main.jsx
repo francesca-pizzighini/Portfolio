9935d2b0809aee20368758e0f3626e97afa61ab0
francesca-pizzighini <133757414+francesca-pizzighini@users.noreply.github.com>
2024-04-05 13:03:56 +0200

    error page ok

--- src/main.jsx
+++ src/main.jsx
@@ -1,48 +1,52 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.scss'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.scss";
 
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-import PageContainer from './pages/pageContainer/pageContainer.jsx'
-import App from './App.jsx'
-import Skills from './pages/skills/skills.jsx'
-import Projects from './pages/projects.jsx'
-import Contacts from './pages/contacts/contacts.jsx'
-import ErrorPage from './pages/errorPage.jsx'
+import PageContainer from "./pages/pageContainer/pageContainer.jsx";
+import App from "./App.jsx";
+import Skills from "./pages/skills/skills.jsx";
+import Projects from "./pages/projects/projects.jsx";
+import Contacts from "./pages/contacts/contacts.jsx";
+import ErrorPage from "./pages/errorPage/errorPage.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <PageContainer/>,
-    // errorElement: <ErrorPage/>
+    element: <PageContainer />,
+    errorElement: <ErrorPage />,
     children: [
       {
         path: "/home",
-        element: <App/>,
-        // errorElement: <ErrorPage/>
+        element: <App />,
+        errorElement: <ErrorPage />,
       },
       {
         path: "/skills",
-        element: <Skills/>,
-        // errorElement: <ErrorPage/>
+        element: <Skills />,
+        errorElement: <ErrorPage />,
       },
       {
         path: "/projects",
-        element: <Projects/>,
-        // errorElement: <ErrorPage/>
+        element: <Projects />,
+        errorElement: <ErrorPage />,
       },
       {
         path: "/contacts",
-        element: <Contacts/>,
-        // errorElement: <ErrorPage/>
-      }
-    ]
+        element: <Contacts />,
+        errorElement: <ErrorPage />,
+      },
+    ],
   },
-])
+  {
+    path: "*",
+    element: <ErrorPage />,
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
---
+++ src/pages/errorPage/errorPage.jsx
@@ -0,0 +1,46 @@
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import Header from "../../components/header/header";
+import Navbar from "../../components/navbar/navbar";
+import "./errorPage.scss";
+
+function ErrorPage() {
+  return (
+    <div className="inside">
+      <div className="figure-one"></div>
+      <div className="figure-two"></div>
+      <div className="figure-three"></div>
+      <HelmetProvider>
+        <Helmet>
+          <title>Error | Francesca Pizzighini</title>
+          <meta
+            name="description"
+            content="This is Francesca Pizzighini's portfolio as a front-end developer. You seem to have encountered an error, please follow the link in the page to come back viting the other pages. The projects page show my works, from my first website using Javascript (a simple counter i use myself when i knit), to the last ones which implements React as this one. I hope you'll like my works.
+            The skills page shows what i've studied so far. Feel free to contact me through the link in the contact page if you want to work toghether."
+          ></meta>
+          <meta
+            name="keywords"
+            content="portfolio, web-developer, francesca-pizzighini, front-end-developer, coding, responsive design, react, javascript, html, css, scss, error, error-encountered, error-page"
+          ></meta>
+        </Helmet>
+      </HelmetProvider>
+      <div className="container">
+        <Header />
+        <Navbar />
+        <div className="outlet error">
+          <h3>An error occurred</h3>
+          <p>
+            Something went wrong, please use the menu at the top of the page to
+            navigate to the section of the website you are intrested in. <br />
+            If the error persist please contact me{" "}
+            <a href="https://docs.google.com/forms/d/1bsfxKTCYddKVTGM17P0xZxRE4I43b7DWJayC8zv8iz8/edit">
+              here
+            </a>{" "}
+            and i'll make sure to fix the issue as soon as possible.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ErrorPage;
